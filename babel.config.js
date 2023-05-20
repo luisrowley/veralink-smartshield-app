3d5440432db7002e9a2294709572571d15615198
luisrowley <louisdebenito@gmail.com>
2023-05-20 19:42:21 +0200

    initial build working with vision camera

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  // Reanimated's Babel plugin here:
+  plugins: [
+    [
+      'react-native-reanimated/plugin',
+      {
+        globals: ['__scanCodes'],
+      },
+    ],
+  ],
 };
