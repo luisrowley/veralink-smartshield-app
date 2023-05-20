7884c1ba05c34c5e136e5e72465b3f0d59879014
luisrowley <louisdebenito@gmail.com>
2023-05-20 22:55:00 +0200

    initialized demo scanner component

--- App.tsx
+++ App.tsx
@@ -3,9 +3,7 @@ import {
   StyleSheet,
   Text
 } from 'react-native';
-
-import { Camera, useCameraDevices } from 'react-native-vision-camera';
-import { BarcodeFormat, useScanBarcodes } from 'vision-camera-code-scanner';
+import ScanScreen from './src/scanner/scanner';
 
 function LoadingView(): JSX.Element {
   return (
@@ -14,37 +12,8 @@ function LoadingView(): JSX.Element {
 }
 
 function App(): JSX.Element {
-  const [hasPermission, setHasPermission] = useState(false);
-  const devices = useCameraDevices();
-  const device = devices.back;
-  const [frameProcessor, barcodes] = useScanBarcodes([BarcodeFormat.QR_CODE], {
-    checkInverted: true,
-  });
-
-  useEffect(() => {
-    (async () => {
-      const status = await Camera.requestCameraPermission();
-      setHasPermission(status === 'authorized');
-    })();
-  }, []);
-
-  if (device == null) return <LoadingView />
-  // if (!hasPermission) return <PermissionsView />
   return (
-    <>
-      <Camera
-        style={StyleSheet.absoluteFill}
-        device={device}
-        isActive={true}
-        frameProcessor={frameProcessor}
-        frameProcessorFps={5}
-      />
-      {barcodes.map((barcode, idx) => (
-        <Text key={idx} style={styles.barcodeTextURL}>
-          {barcode.displayValue}
-        </Text>
-      ))}
-    </>
+    <ScanScreen/>
   );
 }
 
--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  // Reanimated's Babel plugin here:
-  plugins: [
-    [
-      'react-native-reanimated/plugin',
-      {
-        globals: ['__scanCodes'],
-      },
-    ],
-  ],
 };
