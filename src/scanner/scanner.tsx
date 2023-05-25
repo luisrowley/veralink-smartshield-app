c8e078a30ebc51399831b3d1b1f4ac790184f19d
luisrowley <louisdebenito@gmail.com>
2023-05-25 13:08:50 +0200

    solved issue with deprecated ViewPropTypes

--- src/scanner/scanner.tsx
+++ src/scanner/scanner.tsx
@@ -1,9 +1,7 @@
 'use strict';
 
-import React, { Component } from 'react';
-
+import React from 'react';
 import {
-  AppRegistry,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -11,34 +9,31 @@ import {
 } from 'react-native';
 
 import QRCodeScanner from 'react-native-qrcode-scanner';
-import { RNCamera } from 'react-native-camera';
 
-class ScanScreen extends Component {
-  onSuccess = (e: { data: string; }) => {
+function ScanScreen(): JSX.Element {
+  const onSuccess = (e: { data: string; }) => {
     Linking.openURL(e.data).catch(err =>
       console.error('An error occured', err)
     );
   };
 
-  render() {
-    return (
-      <QRCodeScanner
-        onRead={this.onSuccess}
-        topContent={
-          <Text style={styles.centerText}>
-            Go to{' '}
-            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
-            your computer and scan the QR code.
-          </Text>
-        }
-        bottomContent={
-          <TouchableOpacity style={styles.buttonTouchable}>
-            <Text style={styles.buttonText}>OK. Got it!</Text>
-          </TouchableOpacity>
-        }
-      />
-    );
-  }
+  return (
+    <QRCodeScanner
+      onRead={onSuccess}
+      topContent={
+        <Text style={styles.centerText}>
+          Go to{' '}
+          <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+          your computer and scan the QR code.
+        </Text>
+      }
+      bottomContent={
+        <TouchableOpacity style={styles.buttonTouchable}>
+          <Text style={styles.buttonText}>OK. Got it!</Text>
+        </TouchableOpacity>
+      }
+    />
+  );
 }
 
 const styles = StyleSheet.create({
