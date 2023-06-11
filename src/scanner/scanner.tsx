4169d05ece318bbc795a9f2d382ae6d1b7dc60e4
luisrowley <louisdebenito@gmail.com>
2023-06-11 18:27:46 +0200

    complete refactor to class component; added timeout between scans.

--- src/scanner/scanner.tsx
+++ src/scanner/scanner.tsx
@@ -1,68 +1,78 @@
 'use strict';
 
-import React, { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
   TouchableOpacity,
-  Linking
 } from 'react-native';
-
 import QRCodeScanner from 'react-native-qrcode-scanner';
 
-function ScanScreen(): JSX.Element {
-  const [websocket, setWebsocket] = useState<WebSocket | null>(null);
+class ScanScreen extends Component<{}, { ws: WebSocket }>  {
+  constructor(props: any) {
+    super(props);
 
-  useEffect(() => {
-    const ws = new WebSocket('ws://192.168.0.38:8999');
-    
-    ws.onopen = () => {
-      console.log('WebSocket connection opened');
-      setWebsocket(ws);
+    this.state = {
+      ws: new WebSocket('ws://192.168.0.38:8999'),
     };
-    
-    ws.onmessage = (event) => {
+  }
+
+  componentDidMount() {
+    console.log('App loaded!');
+    //this.setState({ ws: new WebSocket('ws://192.168.0.38:8999') });
+
+    this.state.ws.onopen = () => {
+      console.log('WebSocket connection opened');
+      //this.setState({ ws: this.ws });
+    };
+
+    this.state.ws.onmessage = (event) => {
       console.log('Received message:', event.data);
       // Process the received message if needed
     };
-    
-    ws.onerror = (error) => {
+
+    this.state.ws.onclose = () => {
+      console.log("closed");
+    };
+
+    this.state.ws.onerror = (error) => {
       console.log('WebSocket error:', error);
       // Handle WebSocket error
     };
-    
-    return () => {
-      // Clean up the WebSocket connection
-      ws.close();
-    };
-  }, []);
+  }
 
-  const onSuccess = (e: { data: string }) => {
-    if (websocket) {
-      // Send WebSocket message to the server
-      const data = JSON.stringify({ "uid": e.data });
+  componentWillUnmount() {
+    console.log("closing websocket...");
+    this.state.ws.close();
+  }
+
+  onSuccess = (e: any) => {
+    if (this.state.ws) {
+      const data = JSON.stringify({ uid: e.data });
       console.log('Message sent:', data);
-      websocket.send(data);
+      this.state.ws.send(data);
     }
   };
 
-  return (
-    <QRCodeScanner
-      onRead={onSuccess}
-      topContent={
-        <Text style={styles.centerText}>
-          Go to{' '}
-          <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
-          your computer and scan the QR code.
-        </Text>
-      }
-      bottomContent={
-        <TouchableOpacity style={styles.buttonTouchable}>
-          <Text style={styles.buttonText}>OK. Got it!</Text>
-        </TouchableOpacity>
-      }
-    />
-  );
+  render() {
+    return (
+      <QRCodeScanner
+        onRead={this.onSuccess}
+        reactivate={true}
+        reactivateTimeout={800}
+        topContent={
+          <Text style={styles.centerText}>Scan any QR Code from 
+            <Text style={styles.textBold}> bridge.veralink.es </Text>
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>Need help?</Text>
+          </TouchableOpacity>
+        }
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -70,19 +80,19 @@ const styles = StyleSheet.create({
     flex: 1,
     fontSize: 18,
     padding: 32,
-    color: '#777'
+    color: '#777',
   },
   textBold: {
     fontWeight: '500',
-    color: '#000'
+    color: '#000',
   },
   buttonText: {
     fontSize: 21,
-    color: 'rgb(0,122,255)'
+    color: 'rgb(0,122,255)',
   },
   buttonTouchable: {
-    padding: 16
-  }
+    padding: 16,
+  },
 });
 
 export default ScanScreen;
